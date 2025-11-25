52c155887b400faf2c6b746749d2b4dff5498202
ysabrina <123482803+ysabrina@users.noreply.github.com>
2025-11-24 23:31:36 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,11 +1,24 @@
 var video;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+	console.log("Good job opening the window");
 
+	video = document.getElementById("player1");
+    video.loop = false;
+    document.querySelector("#volume").textContent = (video.volume * 100) + "%";
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+ 	console.log("Play Video");
+	video.play();
+ });
+ 
+document.querySelector("#pause").addEventListener("click", function() {
+ 	console.log("Pause Video");
+	video.pause();
+ });
 
+document.querySelector("#slower").addEventListener("click", function() {
+    video.playbackRate *= 0.9;
+    console.log("New speed is:", video.playbackRate.toFixed(5));
+});
