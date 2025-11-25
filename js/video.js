b0e809fd3acd49e393b9f3b003e2e75b40c6ff9d
ysabrina <123482803+ysabrina@users.noreply.github.com>
2025-11-24 23:49:11 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -28,3 +28,43 @@ document.querySelector("#faster").addEventListener("click", function() {
     video.playbackRate /= 0.9;
     console.log("New speed:", video.playbackRate.toFixed(5));
 });
+
+document.querySelector("#skip").addEventListener("click", function() {
+    console.log("Original location:", video.currentTime);
+
+    video.currentTime += 10;
+
+    if (video.currentTime >= video.duration) {
+        video.currentTime = 0;
+    }
+
+    console.log("New location:", video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+    video.muted = !video.muted;
+
+    if (video.muted) {
+        this.textContent = "Unmute";
+        console.log("Muted");
+    } else {
+        this.textContent = "Mute";
+        console.log("Unmuted");
+    }
+});
+
+document.querySelector("#slider").addEventListener("input", function() {
+    video.volume = this.value / 100;
+    document.querySelector("#volume").textContent = (video.volume * 100) + "%";
+    console.log("Volume:", video.volume);
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+    video.classList.add("oldSchool");
+    console.log("Turn Old School version on");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+    video.classList.remove("oldSchool");
+    console.log("Turn Old School version off");
+});
